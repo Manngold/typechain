67b9685548d0f87f06a3a116c5125abd629635fd
Manngold <36980767+Manngold@users.noreply.github.com>
2018-06-25 23:30:03 +0900

    practice typescript
    typescirpt function

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-console.log("hello"); //tsc => make index.js and index.js.map
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const name = "Manngold", age = "25", gender = "Male";
+const sayHi = (name, age, gender) => {
+    console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
+};
+sayHi(name, age); //in typescript you must use arguments
+//tsc => make index.js and index.js.map
+//node.js can't understand typescript so we must change javascript using tsc
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,2 +1,13 @@
-console.log("hello"); //tsc => make index.js and index.js.map
+const name = "Manngold",
+    age = "25",
+    gender ="Male";
+
+const sayHi = (name, age, gender?) => {
+    console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
+};
+
+sayHi(name, age);//in typescript you must use arguments
+
+export {};//this file is module
+//tsc => make index.js and index.js.map
 //node.js can't understand typescript so we must change javascript using tsc
