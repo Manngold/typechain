766d76b23d9b0fe2607bbea4c7a63e1800280e76
manngold <jsw4820@gmail.com>
2018-09-17 23:27:47 +0900

    classes on typescript

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,22 @@
-interface Human {
-    name: String!,
-    age: number!,
-    gender: String!,
+class Human {
+    public name: string;
+    public age: number;
+    public gender: string;
+    constructor(name: string, age: number, gender?: string){
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
 }
 
-const person {
-    name: "Manngold",
-    age: 25, 
-    gender: "male",
-}
+const manngold = new Human("manngold", 25, "male");
 
 const sayHi = (person: Human): string => {
-    return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}`
+    return `Hello ${person.name}, you are ${person.age}, you are a ${
+        person.gender}`
 };
 
-console.log(sayHi("Mannglod", 25, "Male"));
+console.log(sayHi(manngold));
 
 export {};//this file is module
 //tsc => make index.js and index.js.map
